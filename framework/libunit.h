f9e82404926d4d53049b8ac4397352ba87b2841d
Akasha <nmougino@student.42.fr>
2017-02-11 16:06:59 +0100

    Update libunit.h

--- framework/libunit.h
+++ framework/libunit.h
@@ -6,19 +6,27 @@
 /*   By: ahamouda <ahamouda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 12:31:30 by ahamouda          #+#    #+#             */
-/*   Updated: 2017/02/11 12:50:51 by ahamouda         ###   ########.fr       */
+/*   Updated: 2017/02/11 14:23:49 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBUNIT_H
 # define LIBUNIT_H
 
+# include <unistd.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <stdlib.h>
+
+# include <stdio.h>
+# include <string.h>
+
 typedef int				(*f_p)(void);
 
 typedef struct			s_unit_test
 {
 	char				*test_name;
-	f_p					test_func;;
+	f_p					test_func;
 	struct s_unit_test	*next;
 }						t_unit_test;
 
@@ -31,8 +39,11 @@ void					load_test(t_unit_test **list, char *name, f_p func);
 /*
 ** execute tous les tests les un a la suite des autres dans l'interface du
 ** Mini-framework.
+** tableau:: OK, KO, SEGV, BUSE, UNKNOW
 */
 
-int						launch_test(t_unit_test **list);
+int						launch_test(t_unit_test *list);
+
+int						display_return(int stat, char *name);
 
 #endif
