4fd91a514bb1e47ab0761574abd52137cc4ff0cb
Akasha <nmougino@student.42.fr>
2017-02-11 20:56:30 +0100

    Remove Travis test modifications after succesfully execution of Travis

--- real_tests/tests/ft_strlen/00_launcher.c
+++ real_tests/tests/ft_strlen/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 16:39:18 by nmougino          #+#    #+#             */
-/*   Updated: 2017/02/11 20:50:26 by nmougino         ###   ########.fr       */
+/*   Updated: 2017/02/11 20:55:48 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int ft_strlen_launcher(void)
 	list = NULL;
 	ft_printf("\033[1mFT_STRLEN\033[0m\n");
 	load_test(&list, "Basic test", &ft_strlen_basic_test);
-	//load_test(&list, "NULL test", &ft_strlen_null_test);
+	load_test(&list, "NULL test", &ft_strlen_null_test);
 	load_test(&list, "Empty string", &ft_strlen_empty_string);
 	return(launch_test(list));
 }
