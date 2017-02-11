6f5cf8c924e0bf34b8d23c56e18e72c02730ed8a
Akasha <nmougino@student.42.fr>
2017-02-11 20:46:59 +0100

    Fix & norme main_test

--- real_tests/tests/main.c
+++ real_tests/tests/main.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 18:50:28 by nmougino          #+#    #+#             */
-/*   Updated: 2017/02/11 19:09:12 by nmougino         ###   ########.fr       */
+/*   Updated: 2017/02/11 20:46:40 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ int	main(void)
 	int	tab[2];
 
 	ft_bzero(tab, 2 * sizeof(int));
-
 	tab[ft_strlen_launcher() + 1]++;
-
 	ft_printf("%d functionnal - %d crashing - %d total\n", tab[1], tab[0],
 	tab[0] + tab[1]);
-	return (0);
+	return (tab[0] ? -1 : 0);
 }
