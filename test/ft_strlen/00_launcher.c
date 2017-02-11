ab15615cd0e4b1e5399a70d9d08dd192fc3d8878
Akasha <nmougino@student.42.fr>
2017-02-11 16:44:51 +0100

    Base skeleton & ft_strlen unit tests

---
+++ test/ft_strlen/00_launcher.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   00_launcher.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/11 16:39:18 by nmougino          #+#    #+#             */
+/*   Updated: 2017/02/11 16:42:30 by nmougino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+#include "unit_test.h"
+
+int strlen_launcher(void)
+{
+	t_unit_test *testlist;
+
+	ft_printf("FT_STRLEN\n");
+	load_test(&testlist, "Basic test", &basic_test);
+	load_test(&testlist, "NULL test", &null_test);
+	load_test(&testlist, "Empty string", &empty_string);
+	return(launch_tests(&testlist));
+}
---
+++ test/unit_tests.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unit_tests.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/11 16:40:13 by nmougino          #+#    #+#             */
+/*   Updated: 2017/02/11 16:44:24 by nmougino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UNIT_TEST_H
+# define UNIT_TEST_H
+
+/*
+** FT_STRLEN
+*/
+
+int	basic_test(void);
+int	null_test(void);
+int	empty_string(void);
+
+#endif
